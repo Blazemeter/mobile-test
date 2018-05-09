52014921469c79131113b1de088ac8a42c875c6d
Guy Arye <arye.guy@gmail.com>
2018-05-09 11:35:05 +0300

    PEP8

--- mobile-tester.py
+++ mobile-tester.py
@@ -46,7 +46,7 @@ def download_file(url):
     is_flag=True)
 def run(api_key, api_secret, name, silent):
     """Simple program that runs the given mobile tests on blazemeter platform."""
-    #click.echo('All passed in variables %s!' % locals())
+    # click.echo('All passed in variables %s!' % locals())
     app_path = 'resources/BlazeDemo.ipa'
     # 'resources/test_bundle_2018_02_21_21_14.zip'
     test_files_path = 'test-files/python/test_bundle.zip'
@@ -58,13 +58,13 @@ def run(api_key, api_secret, name, silent):
 
     # setup the base url and basic auth
     auth = requests.auth.HTTPBasicAuth(api_key, api_secret)
-    BASE_URL = '{}/api/v4'.format(
+    base_url = '{}/api/v4'.format(
         os.environ.get('BZA_URL', 'https://a.blazemeter.com'))
 
     # creating a test project with name...
     logging.debug("Getting user data for extraction of the project id")
     user_data = requests.get(
-        url='{}/user'.format(BASE_URL), auth=auth).json()['result']
+        url='{}/user'.format(base_url), auth=auth).json()['result']
     default_project_id = user_data['defaultProject']['id']
     logging.debug("Extracted project id %d", default_project_id)
 
@@ -84,7 +84,7 @@ def run(api_key, api_secret, name, silent):
     logging.info("Creating test project with name: %s", project_name)
 
     response = requests.post(
-        url='{}/tests'.format(BASE_URL),
+        url='{}/tests'.format(base_url),
         data=json.dumps(data),
         auth=auth,
         headers={
@@ -95,12 +95,12 @@ def run(api_key, api_secret, name, silent):
     logging.info("Project created...id %d", test_id)
     logging.debug("Getting upload links")
     requests.post(
-        url='{}/tests/{}/mobile-tests/start-uploads'.format(BASE_URL, test_id),
+        url='{}/tests/{}/mobile-tests/start-uploads'.format(base_url, test_id),
         auth=auth)
     got_upload_links = False
     while not got_upload_links:
         test_object = requests.get(
-            url='{}/tests/{}'.format(BASE_URL, test_id),
+            url='{}/tests/{}'.format(base_url, test_id),
             auth=auth).json()['result']
         mobile_test_configuration = test_object['configuration']['plugins'][
             'mobileTest']
@@ -135,13 +135,13 @@ def run(api_key, api_secret, name, silent):
     while not app_upload_succeeded:
         requests.post(
             url='{}/tests/{}/mobile-tests/check-uploads'.format(
-                BASE_URL, test_id),
+                base_url, test_id),
             auth=auth,
             params={
                 "fileType": "app"
             })
         test_object = requests.get(
-            url='{}/tests/{}'.format(BASE_URL, test_id),
+            url='{}/tests/{}'.format(base_url, test_id),
             auth=auth).json()['result']
         mobile_test_configuration = test_object['configuration']['plugins'][
             'mobileTest']
@@ -170,13 +170,13 @@ def run(api_key, api_secret, name, silent):
     while not test_package_upload_succeeded:
         requests.post(
             url='{}/tests/{}/mobile-tests/check-uploads'.format(
-                BASE_URL, test_id),
+                base_url, test_id),
             auth=auth,
             params={
                 "fileType": "testPackage"
             })
         test_object = requests.get(
-            url='{}/tests/{}'.format(BASE_URL, test_id),
+            url='{}/tests/{}'.format(base_url, test_id),
             auth=auth).json()['result']
         mobile_test_configuration = test_object['configuration']['plugins'][
             'mobileTest']
@@ -190,19 +190,18 @@ def run(api_key, api_secret, name, silent):
     # starting the test...
     logging.info("You are all set..Starting the test")
     master_object = requests.post(
-        url='{}/tests/{}/start'.format(BASE_URL, test_id),
+        url='{}/tests/{}/start'.format(base_url, test_id),
         auth=auth).json()['result']
     session_id = master_object['sessionsId'][0]
     logging.info("Started the test with session id %s", session_id)
     artifacts = []
-    got_logs = False
 
     logging.info("Running tests")
     # waiting for the results
     while len(artifacts) == 0:
         artifacts = requests.get(
             url='{}/sessions/{}/mobile-test-artifacts'.format(
-                BASE_URL, session_id),
+                base_url, session_id),
             auth=auth).json()['result']
         print('.')
         time.sleep(SLEEP_TIME + 4 + 10)
